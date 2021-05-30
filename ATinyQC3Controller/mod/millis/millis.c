450a31647768c993c9e675c9fbf1de04b836e727
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-30 01:40:04 -0500

    better millis

--- ATinyQC3Controller/mod/millis/millis.c
+++ ATinyQC3Controller/mod/millis/millis.c
@@ -8,18 +8,23 @@
 #include "../mod.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <util/atomic.h>
 #include <inttypes.h>
 
 uint16_t millis_snap = 0;
 uint16_t ms = 0;
 uint16_t us = 0;
 
-ISR(TIM0_OVF_vect) {
-    us += 2000000000 / F_CPU;
-    while (us > 1000) {
-        ms += us / 1000;
-        us %= 1000;
-    }
+//ISR(TIM0_OVF_vect) {
+    //us += 2000000000 / F_CPU; // Not sure exactly why, but this seems to be working @ 1Mhz
+    //while (us > 1000) {
+        //ms += us / 1000;
+        //us %= 1000;
+    //}
+//}
+
+ISR(TIMER0_COMPA_vect) {
+	ms++;
 }
 
 uint16_t millis(void) {
@@ -45,7 +50,14 @@ void millis_TakeSnap(uint16_t future) {
     millis_snap = future + millis();
 }
 
-void millis_Init(void) {
-    TCCR0B |= (1 << CS01); // 1/8th clock rate
-    TIMSK |= (1 << TOIE0); // Enable timer overflow interrupt
+void millis_Init(void) { // TODO: Increase prescaler as F_CPU increases
+	OCR0A = ((F_CPU / 1000) / 8); // 125 @ 1 Mhz
+	
+	TCCR0A  |= (1 << WGM01); // Clear when matching (CTC)
+	TCCR0B |= (1 << CS01); // 1/8th clock rate
+	
+	TIMSK |= (1 << OCIE0A); // Enable Counter0 Output Compare Match A interrupt
+	
+    // TCCR0B |= (1 << CS01); // 1/8th clock rate
+    // TIMSK |= (1 << TOIE0); // Enable timer overflow interrupt
 }
