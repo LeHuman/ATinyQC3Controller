7db3d903161f80bacab1a8e13572fedf02de29d1
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-30 01:40:18 -0500

    remove comments

--- ATinyQC3Controller/mod/millis/millis.c
+++ ATinyQC3Controller/mod/millis/millis.c
@@ -15,14 +15,6 @@ uint16_t millis_snap = 0;
 uint16_t ms = 0;
 uint16_t us = 0;
 
-//ISR(TIM0_OVF_vect) {
-    //us += 2000000000 / F_CPU; // Not sure exactly why, but this seems to be working @ 1Mhz
-    //while (us > 1000) {
-        //ms += us / 1000;
-        //us %= 1000;
-    //}
-//}
-
 ISR(TIMER0_COMPA_vect) {
 	ms++;
 }
@@ -57,7 +49,4 @@ void millis_Init(void) { // TODO: Increase prescaler as F_CPU increases
 	TCCR0B |= (1 << CS01); // 1/8th clock rate
 	
 	TIMSK |= (1 << OCIE0A); // Enable Counter0 Output Compare Match A interrupt
-	
-    // TCCR0B |= (1 << CS01); // 1/8th clock rate
-    // TIMSK |= (1 << TOIE0); // Enable timer overflow interrupt
 }
